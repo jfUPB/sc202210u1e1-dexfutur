70975fbd2c721287b6fd8341196a36686a42e391
dexfutur <juanp.ruizg@upb.edu.co>
2022-02-13 19:53:45 -0500

    2

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,25 @@
 // Juan Pablo Ruiz Gil
 // 000427707
 // juanp.ruizg@upb.edu.co
+
+
 (START)
-    @START
+
+   @16384
+   D=A
+   @1
+   M=D
+   @1
+   D=M
+   @2
+   M=D
+   @24576
+   D=M
+   @
+
+   @
+
+   @
+
+
     0;JMP
